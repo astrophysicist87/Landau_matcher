8309c77a47684e7875c12e326968536fa604f776
Christopher Plumberg <plumberg.1@osu.edu>
2020-12-16 10:54:24 -0600

    Update transpose_Tmunu.py

--- transpose_Tmunu.py
+++ transpose_Tmunu.py
@@ -2,16 +2,17 @@
 
 import numpy as np
 import sys
-import os.path
+#import os.path
 
-if len(sys.argv) != 4:
-    print "Usage: python transpose_Tmunu.py [filename] [x grid size] [y grid size]"
+if len(sys.argv) != 5:
+    print "Usage: python transpose_Tmunu.py [filename] [x grid size] [y grid size] [new filename]"
 else:
     filename = sys.argv[1]
     xGridSize = int(sys.argv[2])
     yGridSize = int(sys.argv[3])
     
-    newfilename = os.path.dirname(filename) + '/transposed_' + os.path.basename(filename) 
+    #newfilename = os.path.dirname(filename) + '/transposed_' + os.path.basename(filename) 
+    newfilename = sys.argv[4]
     
     data = np.loadtxt(filename)
     dataShape = data.shape
